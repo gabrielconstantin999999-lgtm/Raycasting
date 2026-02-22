ecef4c660f693cb15ac51b2d63d8630ad48776d1
Gabriel Constantin <gabriel.constantin999999@gmail.com>
2026-02-22 21:21:50 +0100

    finished up horizontal checking

--- ray.py
+++ ray.py
@@ -7,6 +7,7 @@ def distance(x1, x2, y1, y2):
     return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 class Ray:
     def __init__(self):
+        self.horizontal_point = None
         self.wall_horizontal_x = None
         self.wall_horizontal_y = None
         self.wall_vertical_x = None
@@ -14,129 +15,36 @@ class Ray:
         self.closest_distance = None
         self.closest_point = None
     def detect_walls(self, screen, px, py, pangle, pdx, pdy, map):
-        print(pdy)
-        tan = math.tan(pangle)
-        if tan == 0:
-            tan = 0.000001 
-        found_horizontal = False
+        tan = abs(math.tan(pangle))
+        print("start")
+        found_h = False
+        h_x_counter = px
+        h_y_counter = py
         if pdy == 'up':
-            o = py % TILESIZE
-            a = o / tan
-            hpx = px + a 
-            hpy = py - o
-            while found_horizontal == False:
-                print(f"grid coords: {int(hpx // TILESIZE - 1), int(hpy//TILESIZE)}")
-                print(f"point: {hpx, hpy}")
-                if map.has_wall_at(int(hpy // TILESIZE - 1), int(hpx//TILESIZE)) == True:
-                    self.wall_horizontal_x = hpx
-                    self.wall_horizontal_y = hpy
-                    found_horizontal = True
+            opposite = py % TILESIZE
+            adjacent = opposite / tan
+            if pdx == 'right':
+                h_x_counter += adjacent
+            elif pdx == 'left':
+                h_x_counter -= adjacent
+            h_y_counter -= opposite
+            print(f"adjacent:{adjacent}")
+            print(f"opposite:{opposite}")
+            while found_h == False:
+                print("new loop")
+                if map.has_wall_at(int(h_y_counter//TILESIZE) - 1, int(h_x_counter//TILESIZE)):
+                    horizontal_point = (h_x_counter, h_y_counter)
+                    print(f"hp:{horizontal_point}")
+                    print(int(h_x_counter//TILESIZE),int(h_y_counter//TILESIZE) - 1)
+                    found_h = True 
                 else:
-                    hpy -= TILESIZE
-
-        if pdy == 'down':
-            o = TILESIZE - (py % TILESIZE)
-            a = o / tan
-            hpx = px + a 
-            hpy =  py + o
-            while found_horizontal == False:
-                print(f"grid coords: {int(hpx // TILESIZE), int(hpy//TILESIZE)}")
-                print(f"point: {hpx, hpy}")
-                if map.has_wall_at(int(hpy // TILESIZE), int(hpx//TILESIZE)) == True:
-                    self.wall_horizontal_x = hpx
-                    self.wall_horizontal_y = hpy
-                    found_horizontal = True
-                else:
-                    hpy += TILESIZE
-        if self.wall_horizontal_x is not None:
-            horizontal_distance = distance(px,self.wall_horizontal_x,py,self.wall_horizontal_y)
-
-        if self.wall_vertical_x is not None:
-            vertical_distance = distance(px,self.wall_vertical_x,py,self.wall_vertical_y)
-
-        self.closest_distance = min(horizontal_distance, vertical_distance)
-
-
-        
-        if horizontal_distance < vertical_distance:
-            self.closest_point = (self.wall_horizontal_x,self.wall_horizontal_y)
-        else:
-            self.closest_point = (self.wall_vertical_x,self.wall_vertical_y)
-
-    
-        found_vertical = False
-        if pdx == 'left':
-            o = px % TILESIZE
-            a = o / (math.tan(pangle))
-            vpx = px + o
-            vpy = py - a
-            while found_vertical == False:
-                print(f"grid coords: {int(vpx // TILESIZE ), int(vpy//TILESIZE-1)}")
-                print(f"point: {vpx, vpy}")
-                if map.has_wall_at(int(vpy // TILESIZE ), int(vpx//TILESIZE -1)) == True:
-                    self.wall_vertical_x = vpx
-                    self.wall_vertical_y = vpy
-                    found_vertical = True
-                else:
-                    vpx -= TILESIZE
-
-        if pdx == 'right':
-            o = TILESIZE - (px % TILESIZE)
-            a = o / (math.tan(pangle))
-            vpx = px + o
-            vpy =  py + a
-            while found_vertical == False:
-                print(f"grid coords: {int(vpx // TILESIZE), int(vpy//TILESIZE)}")
-                print(f"point: {vpx, vpy}")
-                if map.has_wall_at(int(vpy // TILESIZE), int(vpx//TILESIZE)) == True:
-                    self.wall_vertical_x = vpx
-                    self.wall_vertical_y = vpy
-                    found_vertical = True
-                else:
-                    vpx += TILESIZE
-        if self.wall_horizontal_x is not None:
-            horizontal_distance = distance(px,self.wall_horizontal_x,py,self.wall_horizontal_y)
-
-        if self.wall_vertical_x is not None:
-            vertical_distance = distance(px,self.wall_vertical_x,py,self.wall_vertical_y)
-
-        self.closest_distance = min(horizontal_distance, vertical_distance)
-
-
-        
-        if horizontal_distance < vertical_distance:
-            self.closest_point = (self.wall_horizontal_x,self.wall_horizontal_y)
-        else:
-            self.closest_point = (self.wall_vertical_x,self.wall_vertical_y)
-
-        
-
-
-        
-            if self.wall_horizontal_x is not None:
-                horizontal_distance = distance(px,self.wall_horizontal_x,py,self.wall_horizontal_y)
-
-            if self.wall_vertical_x is not None:
-                vertical_distance = distance(px,self.wall_vertical_x,py,self.wall_vertical_y)
-
-            self.closest_distance = min(horizontal_distance, vertical_distance)
-
-
+                    h_y_counter -= TILESIZE
+                    if pdx == 'right':
+                        h_x_counter += TILESIZE/tan
+                    elif pdx == 'left':
+                        h_x_counter -= TILESIZE/tan
+        pygame.draw.line(screen, (255,255,255), (px,py), (h_x_counter, h_y_counter), 2)
+                
             
-            if horizontal_distance < vertical_distance:
-                self.closest_point = (self.wall_horizontal_x,self.wall_horizontal_y)
-            else:
-                self.closest_point = (self.wall_vertical_x,self.wall_vertical_y)  
-
-        
-
-        
-
-
-
-        
-
-
     def cast(self, screen, px, py, pangle):
         pygame.draw.line(screen,(0,0,255),(px, py), (self.closest_point))
-    
