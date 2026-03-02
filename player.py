97eb59581b13c762d55e5cf9ef43d1e5689cdd0a
Gabriel Constantin <gabriel.constantin999999@gmail.com>
2026-03-02 21:54:27 +0100

    fixed movement

--- player.py
+++ player.py
@@ -10,20 +10,31 @@ class Player:
         self.direction_y = None
         self.direction_x = None
         self.speed = 2
-        
+        self.sens = 0.1
+        self.free_mouse = False
     def update(self,screen):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_ESCAPE]:
+               self.free_mouse = True
+               pygame.mouse.set_visible(True)
+        if self.free_mouse == False:
+                pygame.mouse.set_visible(False)
+                pygame.mouse.set_pos(SCREEN_W/2, SCREEN_H/2)
+                
+        x,y = pygame.mouse.get_rel()
+        prev_x = 0
+        if keys[pygame.K_w]:
                 self.x += math.cos(self.rotation_angle) * self.speed
                 self.y -= math.sin(self.rotation_angle) * self.speed
-        if keys[pygame.K_DOWN]:
+        if keys[pygame.K_s]:
                 self.x -= math.cos(self.rotation_angle) * self.speed
                 self.y += math.sin(self.rotation_angle) * self.speed
-        if keys[pygame.K_RIGHT]:
-                self.rotation_angle += self.rotation
-        if keys[pygame.K_LEFT]:
-                self.rotation_angle -= self.rotation
+        if x >= prev_x:
+                self.rotation_angle += self.rotation * (x - prev_x) * self.sens
+        if x <= prev_x:
+                self.rotation_angle -= self.rotation * (prev_x - x) * self.sens
         self.rotation_angle %= 2 * math.pi
         #pygame.draw.circle(screen, (255,0,0), (self.x, self.y), 10)
+        prev_x = x
 
 
